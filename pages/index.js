4829a3b803c813247a8feb3063cddc57f1e296c0
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-08-23 14:31:18 +0200

    footer

--- pages/index.js
+++ pages/index.js
@@ -103,14 +103,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/sbrcode/jam-beam-site"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by <span className={styles.logo}>Sylvain</span>
         </a>
       </footer>
     </div>
