97d81d07ff218d3774191703f0ff65ae92a10cb1
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-08-24 14:18:29 +0200

    fix-navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import { useState } from "react"
 import NavBar from "../components/navbar"
 
 export default function Home() {
